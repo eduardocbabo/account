97835c1d0a11c9e6a571fee2151963caef0ff214
eduardocbabo <ubuntu@Dudu>
2024-10-25 18:00:41 -0300

    Company+Profile+User+Group+Email2

--- mesa/urls.py
+++ mesa/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('admin/auth/', include('access.urls')),
     path('access/', include('access.urls')),
     
     
